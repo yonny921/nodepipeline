5670bd75a2948db24abb446a1de8f9ea9ef66dfa
Yonny Arias <yonny921@gmail.com>
2024-06-29 14:01:30 -0500

    cambio en el codigo 2

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,9 +2,9 @@ let request = require('supertest');
 let app = require('../src/app');
 
 describe('GET /', () => {
-  it('should return hello world', (done) => {
+  it('should return Hola Mundo', (done) => {
     request(app)
       .get('/')
-      .expect('hello world', done);
+      .expect('Hola Mundo', done);
   });
 });
